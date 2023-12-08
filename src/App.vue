5fef741e01b218b1ea214f8afe537e5a54cd129b
deivid1801 <deividleonardo443@gmail.com>
2023-12-08 16:37:53 -0300

    atualização001

--- src/App.vue
+++ src/App.vue
@@ -34,13 +34,107 @@ import { RouterLink, RouterView } from 'vue-router'
   <div class="progress">
   <div class="progress-bar bg-success" style="width:65%"></div>
 </div>
+<p class="porce">52,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:59%"></div>
 </div>
+<p class="porce">51,90%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:48%"></div>
+</div>
+<p class="porce">51,89%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-success" style="width:30%"></div>
+</div>
+<p class="porce">51,84%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+  
+</div>
+
+
+<div class="retangle2">
+  <h2 class="winrate">Ban Rate</h2>
+  <div class="progress">
+  <div class="progress-bar bg-danger" style="width:29%"></div>
+</div>
+<p class="porce">29,12%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar bg-danger" style="width:16%"></div>
+</div>
+<p class="porce">16,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-danger" style="width:14%"></div>
+</div>
+<p class="porce">14,62%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar bg-danger" style="width:13%"></div>
+</div>
+<p class="porce">13,46%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+  
+
+<div class="retangle3">
+  <h2 class="winrate">Pick Rate</h2>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+<br>
+<div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+  <br>
+  <div class="progress">
+  <div class="progress-bar" style="width:100%"></div>
+</div>
+<p class="porce">52,33%</p>
+<img  class="pick" src="../src/assets/campeao.png" alt="">
+</div>
+
+  
+
+</div>
+
+
+
+
+
+
 
 </template>
 
 
 
 <style scoped>
+
+.pick{
+  filter: brightness(2.1);
+  width: 10%;
+  margin-left: 8.5%;
+  margin-top: -18%;
+  border-radius: 20%;
+}
 
 .img{
   background-repeat: no-repeat, repeat;
@@ -152,7 +246,61 @@ margin-left: 50px;
 margin-top: 20px;
 filter:contrast(5);
 }
-
+.winrate{
+  margin-left: 3.5%;
+  margin-top: 5%;
+  color: #FFF;
+text-align: center;
+font-family: Manjari;
+font-style: normal;
+line-height: normal;
+}
   
+.progress{
+  width: 35%;
+  height: 5%;
+  margin-left: 23%;
+  margin-top: 20%;
+  
+}
 
+.retangle3{
+  width: 500px;
+height: 780px;
+border-radius: 110px;
+background: rgba(187, 210, 255, 0.12);
+position: absolute;
+margin-left: 650px;
+margin-top: -145%;
+filter:contrast(5);
+}
+.retangle2{
+  width: 500px;
+height: 780px;
+border-radius: 110px;
+background: rgba(187, 210, 255, 0.12);
+position: absolute;
+margin-left: 700px;
+margin-top: 20px;
+filter:contrast(5);
+}
+
+.progress{
+  width: 35%;
+  height: 5%;
+  margin-left: 23%;
+  margin-top: 20%;
+  
+}
+
+.porce{
+  margin-left: 63%;
+  margin-top: -6%;
+  color: #000;
+font-family: Farro;
+font-size: 25px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+}
 </style>
